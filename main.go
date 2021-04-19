7c3227cb616e3651cdfdcaa878e5a0a3ef956448
FredPont <fred.softwares@gmail.com>
2021-04-19 15:38:57 +0200

    First commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"lasso/src/ui"
+	//"fmt"
+	//"image"
+	//"image/color"
+
+	//"math"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	//"fyne.io/fyne/v2/canvas"
+	//"fyne.io/fyne/v2/container"
+	//"fyne.io/fyne/v2/theme"
+	//"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Draw")
+	e := ui.NewEditor()
+	e.BuildUI(w)
+	w.Resize(fyne.NewSize(600, 600))
+	w.ShowAndRun()
+}
