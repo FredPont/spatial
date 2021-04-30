9fa01dc83d3539d5bbea86a0e63be2b19a3fb22a
FredPont <fred.softwares@gmail.com>
2021-04-30 14:50:22 +0200

    Rotation preference

--- src/pref/preference.go
+++ src/pref/preference.go
@@ -19,36 +19,47 @@ import (
 
 func BuildPref(a fyne.App) {
 	pref := a.Preferences()
-
 	myWindow := a.NewWindow("Preferences")
 
+	// scaling factor
 	scalingFactor := widget.NewEntry()
 	sf := binding.BindPreferenceFloat("scaleFactor", pref) // set the link to preferences for scaling factor
 	x, _ := sf.Get()                                       // read the preference for scaling factor
 	sftxt := fmt.Sprintf("%.8f", x)                        // convert scaling factor to txt
 	scalingFactor.SetPlaceHolder(sftxt)                    // display the prefence value for scaling factor
-	textArea := widget.NewMultiLineEntry()
 
+	// coordinates +90° rotation : necessary for 10x Genomics
+	r := binding.BindPreferenceBool("rotate", pref) // set the link to preferences for rotation
+	b, _ := r.Get()
+	rot := widget.NewCheck("rotate coordinates +90°", func(value bool) {})
+	rot.SetChecked(b)
+	// create form
 	form := &widget.Form{
 		Items: []*widget.FormItem{ // we can specify items in the constructor
-			{Text: "Scaling Factor", Widget: scalingFactor}},
+			{Text: "Scaling Factor", Widget: scalingFactor},
+			{Text: "Rotate", Widget: rot},
+		},
 		OnSubmit: func() { // optional, handle form submission
+
+			// scaling factor
 			sftxt := scalingFactor.Text
-			sffloat, err := strconv.ParseFloat(sftxt, 64)
-			if err == nil {
-				log.Printf("%T, %v\n", sf, sf)
+			log.Println("SF stored:", scalingFactor.Text)
+			if sftxt != "" {
+				sffloat, err := strconv.ParseFloat(sftxt, 64)
+				if err == nil {
+					log.Printf("%T, %v\n", sf, sf)
+				}
+				pref.SetFloat("scaleFactor", sffloat) // store the new preference for scaling factor
 			}
-			pref.SetFloat("scaleFactor", sffloat) // store the new preference for scaling factor
+
+			// coordinates +90° rotation
+			pref.SetBool("rotate", rot.Checked)
 
 			log.Println("Form submitted:", scalingFactor.Text)
-			log.Println("multiline:", textArea.Text)
 
 			myWindow.Close()
 		},
 	}
-
-	// we can also append items
-	form.Append("Text", textArea)
 
 	myWindow.SetContent(form)
 	myWindow.Show()
